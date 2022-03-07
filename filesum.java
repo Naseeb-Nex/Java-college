6c43d81d185dc7c6a23c9f654c72e007fbc4aab3
Naseeb-Nex <muhammednaseeb02@gmail.com>
2022-03-07 23:07:59 +0530

    Find the sum of number from a txt file

--- filesum.java
+++ filesum.java
@@ -1,30 +1,30 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class filesum {
-    public static void main(String[] args) {
-        try{
-            File f = new File("sum.txt");
-            f.createNewFile();
-            FileWriter fw = new FileWriter("sum.txt");
-            fw.write("11 12 13");
-            fw.close();
 
-            Scanner r = new Scanner(f);
-            int sum = 0;
-            while(r.hasNextLine()){
-                String num = r.nextLine();
-                StringTokenizer st = new StringTokenizer(num);
-                while(st.hasMoreTokens()){
-                    int n = Integer.parseInt(st.nextToken());
-                    sum = sum + n;
-                }
-            }
+  public static void main(String[] args) {
+    try {
+      File f = new File("sum.txt");
+      f.createNewFile();
+      FileWriter fw = new FileWriter("sum.txt");
+      fw.write("11 12 13");
+      fw.close();
 
-            System.out.println("The Sum is : "+sum);
-
-        }catch(Exception e){
-            System.out.println(e);
+      int sum = 0;
+      Scanner sc = new Scanner(f);
+      while (sc.hasNextLine()) {
+        String num = sc.nextLine();
+        StringTokenizer st = new StringTokenizer(num);
+        while (st.hasMoreTokens()) {
+          int n = Integer.parseInt(st.nextToken());
+          sum = sum + n;
         }
+      }
+      System.out.println("The Sum is : " + sum);
+      
+    } catch (Exception e) {
+      System.out.println(e);
     }
+  }
 }
