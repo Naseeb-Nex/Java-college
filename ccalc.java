69676907a2ef1b9e59b7a4322167804d4982d5bf
Naseeb-Nex <muhammednaseeb02@gmail.com>
2022-04-21 23:25:03 +0530

    Custome ui

---
+++ ccalc.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class ccalc extends JFrame {
+    ccalc(){
+        setLayout(null);
+        setSize(230,100);
+    }
+    public static void main(String[] args) {
+        new ccalc().setVisible(true);
+        
+    }
+}
