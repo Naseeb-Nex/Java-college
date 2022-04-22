de3a385aa791a1d7905dda927d570e1a58482fe3
Naseeb-Nex <muhammednaseeb02@gmail.com>
2022-04-22 23:59:10 +0530

    Update

---
+++ traffic.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class traffic {
+    
+    traffic(){
+        JFrame t = new JFrame("Traffic Light");
+        t.setLayout(null);
+        t.setSize(500,300);
+        t.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new traffic();
+    }
+}
